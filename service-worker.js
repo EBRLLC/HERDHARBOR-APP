08648014383e4b8193ea3f550373e8c684474209
EBRLLC <mdamrell@yahoo.com>
2026-07-30 14:31:51 -0400

    Update service-worker.js for v0.2.14

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,13 @@
 "use strict";
 
 const CACHE_PREFIX = "herdharbor-shell-";
-const CACHE_NAME = `${CACHE_PREFIX}v0.2.13-20260730`;
+const CACHE_NAME = `${CACHE_PREFIX}v0.2.14-20260730`;
 const APP_SHELL = [
   "./",
   "./index.html",
   "./manifest.json?v=3",
   "./herdharbor-cloud.js?v=3",
-  "./pwa.js?v=1",
+  "./pwa.js?v=2",
   "./vendor/supabase-2.111.0.js",
   "./icon-192.png",
   "./icon-512.png"
