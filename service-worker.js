285dc483a446db253d970c08330a654728234a1d
EBRLLC <mdamrell@yahoo.com>
2026-07-30 22:51:04 -0400

    Release v0.3.03 tester stability: service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,17 @@
 "use strict";
 
 const CACHE_PREFIX = "herdharbor-shell-";
-const CACHE_NAME = `${CACHE_PREFIX}v0.3.02-20260731`;
+const CACHE_NAME = `${CACHE_PREFIX}v0.3.03-20260731`;
 const APP_SHELL = [
   "./",
   "./index.html",
   "./manifest.json?v=5",
-  "./herdharbor-cloud.js?v=4",
+  "./herdharbor-cloud.js?v=5",
   "./pwa.js?v=5",
   "./vendor/supabase-2.111.0.js",
   "./vendor/jszip-3.10.1.min.js",
   "./vendor/exceljs-4.4.0.min.js",
-  "./spreadsheet-import.js?v=2",
+  "./spreadsheet-import.js?v=3",
   "./icon-192.png",
   "./icon-512.png"
 ];
