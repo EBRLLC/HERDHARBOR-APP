e0f170d3e5d3c95966b73de4b9e44aa444c3e8e9
EBRLLC <mdamrell@yahoo.com>
2026-08-31 18:40:52 -0700

    Bump PWA cache for v1.6.1 genetics hotfix

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const CACHE_PREFIX = "herdharbor-shell-";
-const CACHE_NAME = `${CACHE_PREFIX}v1.6.1-alpha-current-state-1`;
+const CACHE_NAME = `${CACHE_PREFIX}v1.6.1-alpha-current-state-2`;
 const APP_SHELL = [
   "./",
   "./index.html",
