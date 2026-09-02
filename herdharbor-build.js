0abfff585b1c3be3dc391e34634247384bb794e5
EBRLLC <mdamrell@yahoo.com>
2026-09-02 11:15:45 -0400

    Advance completion release identity to Alpha v1.6.7

--- herdharbor-build.js
+++ herdharbor-build.js
@@ -3,8 +3,8 @@
   root.HerdHarborBuild = Object.freeze({
     product: "HerdHarbor",
     channel: "Alpha",
-    version: "1.6.6",
+    version: "1.6.7",
     buildId: "completion-debt-1",
-    build: "1.6.6-alpha-completion-debt-1"
+    build: "1.6.7-alpha-completion-debt-1"
   });
 })(typeof globalThis !== "undefined" ? globalThis : this);
