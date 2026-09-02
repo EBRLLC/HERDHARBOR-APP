974cd6d755392ca86599ab9f09408638c4f95b2f
EBRLLC <mdamrell@yahoo.com>
2026-09-02 05:36:58 -0700

    Bump HerdHarbor build identity to Alpha v1.6.6

--- herdharbor-build.js
+++ herdharbor-build.js
@@ -3,8 +3,8 @@
   root.HerdHarborBuild = Object.freeze({
     product: "HerdHarbor",
     channel: "Alpha",
-    version: "1.6.5",
-    buildId: "analytics-market-foundation-1",
-    build: "1.6.5-alpha-analytics-market-foundation-1"
+    version: "1.6.6",
+    buildId: "completion-debt-1",
+    build: "1.6.6-alpha-completion-debt-1"
   });
 })(typeof globalThis !== "undefined" ? globalThis : this);
